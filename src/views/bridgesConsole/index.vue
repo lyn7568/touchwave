bfbb587d25c0c78e69dd232d03c6578430e74d96
luyanan <1271778060@qq.com>
2018-08-22 13:08:47 +0800

    用户主页

---
+++ src/views/bridgesConsole/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <el-menu class="navbar" mode="horizontal">
+      <breadcrumb></breadcrumb>
+    </el-menu>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+import Breadcrumb from '@/components/Breadcrumb'
+
+export default {
+  name: 'bridgeHome',
+  components: {
+    Breadcrumb
+  }
+}
+</script>
