aa3bbb4381c8e8c4a841f662322acf9ab378653c
luyanan <1271778060@qq.com>
2018-09-18 15:27:00 +0800

    IE show

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 
 import 'normalize.css/normalize.css'// A modern alternative to CSS resets
@@ -6,7 +7,6 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 import '@/styles/index.scss' // global css
-import 'babel-polyfill'
 
 import App from './App'
 import router from './router'
